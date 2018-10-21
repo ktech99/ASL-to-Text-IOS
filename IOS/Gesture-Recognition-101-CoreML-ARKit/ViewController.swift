51fa678812a213a51a1fdfa6e9d65f21ed2093ad
Kha Nguyen <kha.vng@gmail.com>
2018-10-21 11:12:56 -0700

    Greatly improved detection algorithm

--- IOS/Gesture-Recognition-101-CoreML-ARKit/ViewController.swift
+++ IOS/Gesture-Recognition-101-CoreML-ARKit/ViewController.swift
@@ -12,147 +12,166 @@ import ARKit
 import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-@IBOutlet var sceneView: ARSCNView!
-@IBOutlet weak var debugTextView: UITextView!
-@IBOutlet weak var textOverlay: UITextField!
-
-let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
-var visionRequests = [VNRequest]()
-
-override func viewDidLoad() {
-    super.viewDidLoad()
     
-    // --- ARKIT ---
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var textOverlay: UITextField!
     
-    // Set the view's delegate
-    sceneView.delegate = self
+    let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
+    var visionRequests = [VNRequest]()
     
-    // Show statistics such as fps and timing information
-    sceneView.showsStatistics = true
+    var gestureFrequenciesMap = [String: Int]()
+    var gestureFrequenciesSize = 0
+    var maxChar = "❎"
     
-    // Create a new scene
-    let scene = SCNScene() // SCNScene(named: "art.scnassets/ship.scn")!
-    
-    // Set the scene to the view
-    sceneView.scene = scene
-    
-    // --- ML & VISION ---
-    
-    // Setup Vision Model
-    guard let selectedModel = try? VNCoreMLModel(for: example_5s0_hand_model().model) else {
-        fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project. Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // --- ARKIT ---
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+        let scene = SCNScene() // SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // --- ML & VISION ---
+        
+        // Setup Vision Model
+        guard let selectedModel = try? VNCoreMLModel(for: example_5s0_hand_model().model) else {
+            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project. Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
+        }
+        
+        // Set up Vision-CoreML Request
+        let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
+        visionRequests = [classificationRequest]
+        
+        // Begin Loop to Update CoreML
+        loopCoreMLUpdate()
     }
     
-    // Set up Vision-CoreML Request
-    let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
-    classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
-    visionRequests = [classificationRequest]
-    
-    // Begin Loop to Update CoreML
-    loopCoreMLUpdate()
-}
-
-override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    
-    // Create a session configuration
-    let configuration = ARWorldTrackingConfiguration()
-
-    // Run the view's session
-    sceneView.session.run(configuration)
-}
-
-override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    
-    // Pause the view's session
-    sceneView.session.pause()
-}
-
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Release any cached data, images, etc that aren't in use.
-}
-
-// MARK: - ARSCNViewDelegate
-
-func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-    DispatchQueue.main.async {
-        // Do any desired updates to SceneKit here.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
     }
-}
-
-// MARK: - MACHINE LEARNING
-
-func loopCoreMLUpdate() {
-    // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
-    dispatchQueueML.async {
-        // 1. Run Update.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            // Do any desired updates to SceneKit here.
+        }
+    }
+    
+    // MARK: - MACHINE LEARNING
+    
+    func loopCoreMLUpdate() {
+        // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
+        dispatchQueueML.async {
+            // 1. Run Update.
             self.updateCoreML()
-        // 2. Loop this function.
+            // 2. Loop this function.
             self.loopCoreMLUpdate()
-    }
-}
-
-func updateCoreML() {
-    // Get Camera Image as RGB
-    let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
-    if pixbuff == nil { return }
-    let ciImage = CIImage(cvPixelBuffer: pixbuff!)
-    
-    // Prepare CoreML/Vision Request
-    let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
-    
-    // Run Vision Image Request
-    do {
-        try imageRequestHandler.perform(self.visionRequests)
-    } catch {
-        print(error)
-    }
-}
-
-func classificationCompleteHandler(request: VNRequest, error: Error?) {
-    // Catch Errors
-    if error != nil {
-        print("Error: " + (error?.localizedDescription)!)
-        return
-    }
-    guard let observations = request.results else {
-        print("No results")
-        return
+        }
     }
     
-    // Get Classifications
-    let classifications = observations.prefix(3) // top 3 results
-        .compactMap({ $0 as? VNClassificationObservation })
-        .map({ "\($0.identifier) \(String(format:" : %.2f", $0.confidence))" })
-        .joined(separator: "\n")
-    // Render Classifications
-    DispatchQueue.main.async {
-        // Print Classifications
+    func updateCoreML() {
+        // Get Camera Image as RGB
+        let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
+        if pixbuff == nil { return }
+        let ciImage = CIImage(cvPixelBuffer: pixbuff!)
+        
+        // Prepare CoreML/Vision Request
+        let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        // Run Vision Image Request
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+    }
+    func getMaxChar(computedPrediction: String) {
+        gestureFrequenciesSize += 1
+        if (gestureFrequenciesSize > 30) {
+            gestureFrequenciesSize = 0
+            var max = 0
+            for (prediction, frequency) in gestureFrequenciesMap {
+                if (frequency > max) {
+                    max = frequency
+                    maxChar = prediction
+                }
+            }
+            gestureFrequenciesMap.removeAll()
+        } else {
+            if gestureFrequenciesMap.keys.contains(computedPrediction) {
+                gestureFrequenciesMap[computedPrediction] = gestureFrequenciesMap[computedPrediction]! + 1
+            } else {
+                gestureFrequenciesMap[computedPrediction] = 1
+            }
+        }
+    }
+    
+    func classificationCompleteHandler(request: VNRequest, error: Error?) {
+        // Catch Errors
+        if error != nil {
+            print("Error: " + (error?.localizedDescription)!)
+            return
+        }
+        guard let observations = request.results else {
+            print("No results")
+            return
+        }
+        
+        // Get Classifications
+        let classifications = observations.prefix(3) // top 3 results
+            .compactMap({ $0 as? VNClassificationObservation })
+            .map({ "\($0.identifier) \(String(format:" : %.2f", $0.confidence))" })
+            .joined(separator: "\n")
+        // Render Classifications
+        DispatchQueue.main.async {
+            // Print Classifications
             // print(classifications)
             // print("-------------")
-        
-        // Display Debug Text on screen
-        self.debugTextView.text = "TOP 3 PROBABILITIES: \n" + classifications
-        
-        // Display Top Symbol
-        var symbol = "❎"
-        let topPrediction = classifications.components(separatedBy: "\n")[0]
-        let topPredictionName = topPrediction.components(separatedBy: ":")[0].trimmingCharacters(in: .whitespaces)
-        // Only display a prediction if confidence is above 1%
-        let topPredictionScore:Float? = Float(topPrediction.components(separatedBy: ":")[1].trimmingCharacters(in: .whitespaces))
-        if (topPredictionScore != nil && topPredictionScore! > 0.01) {
-            if (topPredictionName == "hi") { symbol = "hi" }
-            if (topPredictionName == "love") { symbol = "I love you" }
-            if (topPredictionName == "negative") { symbol = "❎" }
+            
+            
+            // Display Top Symbol
+            var symbol = "❎"
+            let topPrediction = classifications.components(separatedBy: "\n")[0]
+            let topPredictionName = topPrediction.components(separatedBy: ":")[0].trimmingCharacters(in: .whitespaces)
+            // Only display a prediction if confidence is above 1%
+            let topPredictionScore:Float? = Float(topPrediction.components(separatedBy: ":")[1].trimmingCharacters(in: .whitespaces))
+            
+            if (topPredictionScore != nil && topPredictionScore! > 0.01) {
+                self.getMaxChar(computedPrediction: topPredictionName)
+                if (self.maxChar.contains("hi")) { symbol = "hi" }
+                if (self.maxChar.contains("love")) { symbol = "I love you" }
+                if (self.maxChar == "negative") { symbol = "❎" }
+            }
+            self.textOverlay.text = symbol
         }
-        self.textOverlay.text = symbol
-        
     }
-}
-
-// MARK: - HIDE STATUS BAR
-override var prefersStatusBarHidden : Bool { return true }
+    
+    // MARK: - HIDE STATUS BAR
+    override var prefersStatusBarHidden : Bool { return true }
 }
