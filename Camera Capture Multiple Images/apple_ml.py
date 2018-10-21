ac70b0d1899837a7196dc06183f943080ffab941
Kha Nguyen <kha.vng@gmail.com>
2018-10-21 09:52:34 -0700

    Working right hand model

--- Camera Capture Multiple Images/apple_ml.py
+++ Camera Capture Multiple Images/apple_ml.py
@@ -9,7 +9,7 @@ data = tc.image_analysis.load_images(
 #applying labels to our image (telling turicreate what image showld be labelled as what)
 data['label'] = data['path'].apply(
     #labelling the image as a cat if the folder name is cat, labelling it as a dog if it isn't
-    lambda path: 'hi' if '/hi/' in path else 'love'
+    lambda path: 'hi' if '/hi/' in path else 'love' if '/love/' in path else 'negative'
 )
 
 #counting the number of each label (how many cats and dog images there are)
@@ -17,14 +17,14 @@ data.groupby('label', [tc.aggregate.COUNT])
 
 #Dividing the images randomly in 2 sections with a ratio of 8:2
 #This is important for us to check the accuracy of our model
-training_data, testing_data = data.random_split(0.80)
+training_data, testing_data = data.random_split(0.90)
 
 #Training the model
 model = tc.image_classifier.create(
     training_data,
     target='label',
     #using the squeezenet model for mobiles, for more accurate results use resnet-50
-    model='squeezenet_v1.1',
+    model='resnet-50',
     #number of times turicreate should attempt to train the model
     max_iterations=3000
 )
@@ -34,3 +34,4 @@ model.evaluate(testing_data)
 
 #exporting the model to be used as a mlmodel used by ios
 model.export_coreml('example_5s0_hand_model.mlmodel')
+
