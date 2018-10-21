5c6414a04eb409c31f3c4251e8cf82579573771b
Kha Nguyen <kha.vng@gmail.com>
2018-10-20 17:41:45 -0700

    Update script to put images in /img/gesture_name/person_name/

--- Camera Capture Multiple Images/cam_capture.py
+++ Camera Capture Multiple Images/cam_capture.py
@@ -3,16 +3,21 @@ import time
 import os
 # Create new folder of a certain sequence of images inside the img/ folder
 # Note: Most have img/ folder inside the same directory as cam_capture
-
-file_name = raw_input("Name of the sign language gesture? ")
-path = "img/" + file_name + "/"
-os.mkdir(path)
+if not os.path.exists("img/"):  # Create img/ folder if doesn't already exists
+    os.mkdir("img/")
+sign_name = raw_input("Name of the sign language gesture? ")
+person_name = raw_input("Name of the person? ")
+if not os.path.exists("img/" + sign_name + "/"):
+    os.mkdir("img/" + sign_name + "/")
+path = "img/" + sign_name + "/" + person_name + "/"
+if not os.path.exists(path):
+    os.mkdir(path)
 for timer in range(3,0,-1):
     print(timer)
     time.sleep(1)
 camera = cv2.VideoCapture(0)
 for i in range(15):
     return_value, image = camera.read()
-    cv2.imwrite('img/' + file_name + "/" + file_name + "-" + str(i) + '.png', image)
+    cv2.imwrite('img/' + sign_name + "/" + person_name + "/" + person_name + "-" + str(i) + '.png', image)
     time.sleep(.100)
 camera.release()
